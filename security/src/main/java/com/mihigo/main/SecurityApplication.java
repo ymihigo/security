afd5c4c2e12d9790bbc7305b15ad14b819051f11
MIHIGO Yves <ymihigo70@gmail.com>
2022-05-10 09:16:36 +0200

    init

---
+++ security/src/main/java/com/mihigo/main/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mihigo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
